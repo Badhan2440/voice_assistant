e047b2a75ee34503285245b02e134ac7b03d1469
Badhan2440 <paulbadhan7109@gmail.com>
2022-03-20 07:50:14 +0600

    final commit

--- alexa.py
+++ alexa.py
@@ -1,13 +1,15 @@
 import speech_recognition as sr
 import pyttsx3
 import datetime
-
+import pywhatkit
+import wikipedia
+import pyjokes
 
 listener= sr.Recognizer()
 alexa= pyttsx3.init()
 
-#alexa.say("Hello Paul. How may I help u?")
-#alexa.runAndWait()
+alexa.say("Hello Paul. How may I help u?")
+alexa.runAndWait()
 
 def talk(text):
     alexa.say(text)
@@ -16,13 +18,13 @@ def talk(text):
 def take_command():
     try:
         with  sr.Microphone() as source:
-            print("Ami shuntesi, bolen...")
+            print("Listening...")
             voice= listener.listen(source)
             command= listener.recognize_google(voice)
             command=command.lower()
 
             if 'alexa' in command:
-                print(command)
+                command=command.replace('alexa', '')
 
     except:
         pass
@@ -36,4 +38,33 @@ def run_alexa():
         print(time)
         talk("Current time is "+time)
 
-run_alexa()
+
+    elif 'play' in command:
+        song=command.replace('play', '')
+        talk('playing '+song)
+        pywhatkit.playonyt(song)
+
+
+    elif 'tell me about' in command:
+        info= command.replace('tell me about ', '')
+        info= wikipedia.summary(info, 1)
+        print(info)
+        talk(info)
+
+    
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+
+    elif 'date' in command:
+        talk('Sorry, I have a boyfriend')
+
+    else:
+        talk('I do not know it but google does. let me search it for you.')
+        pywhatkit.search(command)
+
+
+
+
+
+while True:
+    run_alexa()
